0b91f911e475364c87c2369a70784c39c8e673ed
arafferty10 <aira5953@colorado.edu>
2018-12-14 20:59:45 -0700

    printerStories used to test random fxn with button press + print

--- printerStories.py
+++ printerStories.py
@@ -9,7 +9,9 @@ import serial
 import adafruit_thermal_printer
 import RPi.GPIO as GPIO
 import time
+import random
 
+#--------------------SETUP------------------------
 #Set values for button
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -19,13 +21,39 @@ uart = serial.Serial("../../../dev/serial0", baudrate=19200, timeout=3000)
 
 #Create Thermal Printer Class
 ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.68)
-
 printer = ThermalPrinter(uart)
 
-#printer.feed() advances the paper forward some lines given
-printer.feed(5)
+#-------------------------------------------------
 
-printer.print("Hello World! I'm Alive!")
-printer.feed(3)
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        n = 0
+        n = random.randint(1,3)
+        print('Button Pressed')
+        print(n)
+        time.sleep(0.4)
+
+        if n == 1:
+            printer.feed(5)
+            printer.print("Choice 1: Help I'm stuck in the printer")
+        else if n == 2:
+            printer.feed(5)
+            printer.bold = True
+            printer.print('cHoIcE 2: Do or do not there is no try!')
+            printer.bold = False
+        else if n == 3:
+            printer.feed(5)
+            printer.underline = adafruit_thermal_printer.UNDERLINE_THICK
+            printer.bold = True
+            printer.print('#3: I WILL DESTROY YOU!!!')
+            printer.underline = None
+            printer.bold = False
+        else if n == 4:
+            printer.feed(5)
+            printer.double_height = True
+            printer.print('Selection 4: Commander Cody, Execute Order 66!')
+            printer.double_height = False
+
 
 print("Code Complete!")
