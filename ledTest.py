6bc3682e504db987ebb87f87735859cc1be69423
arafferty10 <aira5953@colorado.edu>
2019-04-27 20:23:38 -0400

    fixed for python3

--- ledTest.py
+++ ledTest.py
@@ -3,8 +3,8 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(23,GPIO.OUT)
-print "LED on"
+print("LED on")
 GPIO.output(23,GPIO.HIGH)
 time.sleep(1)
-print "LED off"
+print("LED off")
 GPIO.output(23,GPIO.LOW)
